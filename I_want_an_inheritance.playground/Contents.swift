039bb745ecfea9fd3f146518e0b45edeec70c4d3
SpaceForestChu <spaceforestchu@gmail.com>
2016-07-07 00:53:24 -0400

    Second Commit

--- I_want_an_inheritance.playground/Contents.swift
+++ I_want_an_inheritance.playground/Contents.swift
@@ -21,15 +21,45 @@ class Person {
 var jon = Person(name: "Jon");
 
 class Animorph: Person {
+    
     var animalType = "Dog"
     
     convenience init(type: String, name: String){
         self.init(name: name)
         self.animalType = type
+        self.speed = "25 MPH"
+    }
+    override func catchPhrases() -> String {
+        return "I can turn into a cool animal"
     }
 }
 
 print(jon.eyeColor)
+print(jon.catchPhrases())
 
 var dolphin = Animorph(type: "Dolphin", name: "Jack")
+
+print(dolphin.catchPhrases())
+
+class SuperHero : Person {
+    var specialPower = "X-Ray Vision"
+    var backStory = "Some Truck hit some slime that had chemicals and now I am super awesome"
+    
+    convenience init(name: String, press: String) {
+        self.init(name: name)
+        benchPress = press
+    }
+    
+    override func catchPhrases() -> String {
+        return "Eat my puke"
+    }
+}
+
+
+var pukeMan = SuperHero(name: "Puke Man", press: "5000 LBS")
+
+print(pukeMan.benchPress)
+
+
+
 
